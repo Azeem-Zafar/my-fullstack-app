f58617d0138e412f4a9399631726a0bbe1118cb9
Azeem-Zafar <azeemzafar90@gmail.com>
2025-05-20 18:56:20 +0500

    Serve React build folder from backend

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 // For any other requests, send back React's index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client-dashbaord/build/index.html'));
 });
 
 app.get('/api/projects', async (req, res) => {
